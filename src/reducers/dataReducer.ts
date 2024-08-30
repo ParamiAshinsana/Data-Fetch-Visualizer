0fd2df4548a696f5712be0b67f9ec60699524f39
Parami <paramiashinsana@gmail.com>
2024-08-30 22:36:29 +0530

    update

--- src/reducers/dataReducer.ts
+++ src/reducers/dataReducer.ts
@@ -8,3 +8,14 @@ export default function dataReducer() {
     </View>
   )
 }
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function dataReducer() {
+  return (
+    <View>
+      <Text>dataReducer</Text>
+    </View>
+  )
+}
