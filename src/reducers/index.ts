a5a23afda57e991c362cda83d2f8ce5a58a86b0b
Parami <paramiashinsana@gmail.com>
2024-08-30 22:34:39 +0530

    update

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -8,3 +8,25 @@ export default function index() {
     </View>
   )
 }
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
